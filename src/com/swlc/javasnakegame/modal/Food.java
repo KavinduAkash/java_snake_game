c3c60efd076d3a71f6f3f2b19f8b10563a1991d0
KavinduAkash <bkkasamarasingha1220@gmail.com>
2021-07-22 06:57:06 +0530

    create food modal class
    food handle by this class

---
+++ src/com/swlc/javasnakegame/modal/Food.java
@@ -0,0 +1,59 @@
+package com.swlc.javasnakegame.modal;
+
+import com.swlc.javasnakegame.constant.GameConstant;
+
+import java.util.Random;
+
+/**
+ * @author hp
+ */
+
+/**
+ * This class responsible for handle food
+ */
+public class Food {
+    int appleX;
+    int appleY;
+
+    public Food(Snake snake) { newFood(snake); }
+
+    /**
+     * This method use to generate new food and set food location
+     * Also, restrict show apple on snake
+     * @param snake
+     */
+    public void newFood(Snake snake) {
+        boolean onSnake = true;
+        while(onSnake) {
+            onSnake = false;
+
+            Random random = new Random();
+
+            appleX = random.nextInt((int)(GameConstant.SCREEN_WIDTH/GameConstant.UNIT_SIZE)) * GameConstant.UNIT_SIZE;
+            appleY = random.nextInt((int)(GameConstant.SCREEN_HEIGHT/GameConstant.UNIT_SIZE)) * GameConstant.UNIT_SIZE;
+
+            for(int i = 0; i<snake.getX().length; i++) {
+                if(snake.getX()[i] == appleX && snake.getY()[i] == appleY) {
+                    onSnake = true;
+                }
+            }
+
+        }
+    }
+
+    public int getAppleX() {
+        return appleX;
+    }
+
+    public void setAppleX(int appleX) {
+        this.appleX = appleX;
+    }
+
+    public int getAppleY() {
+        return appleY;
+    }
+
+    public void setAppleY(int appleY) {
+        this.appleY = appleY;
+    }
+}
