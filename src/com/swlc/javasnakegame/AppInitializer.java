62ae8b3ce28b0171fbca88e9479abe5af42bf6d7
KavinduAkash <bkkasamarasingha1220@gmail.com>
2021-07-22 07:13:10 +0530

    create "SnakeGameView" class
    jframe inherit into this class.

--- src/com/swlc/javasnakegame/AppInitializer.java
+++ src/com/swlc/javasnakegame/AppInitializer.java
@@ -1,10 +1,13 @@
 package com.swlc.javasnakegame;
 
+import com.swlc.javasnakegame.view.SnakeGameView;
+
 /**
  * @author hp
  */
 public class AppInitializer {
     public static void main(String[] args) {
         System.out.println("App Initialized!");
+        new SnakeGameView();
     }
 }
---
+++ src/com/swlc/javasnakegame/view/SnakeGameView.java
@@ -0,0 +1,30 @@
+package com.swlc.javasnakegame.view;
+
+import com.swlc.javasnakegame.controller.GameBoard;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * @author hp
+ */
+public class SnakeGameView extends JFrame{
+    public SnakeGameView() {
+        super("Java Snake Game");
+        viewBuild();
+    }
+
+    private void viewBuild() {
+        prepareWindow();
+    }
+
+    private void prepareWindow() {
+        this.setTitle("Java Snake Game");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+
+}
