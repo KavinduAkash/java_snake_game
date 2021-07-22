7b821daf98e385e7332f7431467a031d43445404
KavinduAkash <bkkasamarasingha1220@gmail.com>
2021-07-22 08:30:11 +0530

    implement game start
    game board class extends by runnable interface

--- src/com/swlc/javasnakegame/controller/GameBoard.java
+++ src/com/swlc/javasnakegame/controller/GameBoard.java
@@ -7,12 +7,11 @@ import com.swlc.javasnakegame.view.SnakeGameView;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionListener;
 
 /**
  * @author hp
  */
-public class GameBoard extends JPanel {
+public class GameBoard extends JPanel implements Runnable {
 
     private Snake s;
     private Food f;
@@ -30,7 +29,61 @@ public class GameBoard extends JPanel {
         setBorder(BorderFactory.createMatteBorder(
                 2, 2, 2, 2, Color.black));
 
+        startGame();
+
         this.setPreferredSize(new Dimension(GameConstant.SCREEN_WIDTH, GameConstant.SCREEN_HEIGHT));
         this.setFocusable(true);
     }
+
+    private void startGame() {
+        running = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    @Override
+    public void run() {
+
+        try {
+            while (true) {
+                s.move();
+                if (!running) {
+                    Thread.currentThread().interrupt();
+                }
+                if (!Thread.currentThread().isInterrupted()) {
+                    repaint();
+                }
+                Thread.sleep(GameConstant.DELAY);
+            }
+        } catch (InterruptedException ex) {
+            System.out.println("GAME OVER");
+
+            repaint();
+
+        }
+
+    }
+
+    public void paintComponent(java.awt.Graphics g) {
+
+        super.paintComponent(g);
+
+        if(running) {
+            //apple
+            g.setColor(Color.red);
+            g.fillOval(f.getAppleX(), f.getAppleY(), GameConstant.UNIT_SIZE, GameConstant.UNIT_SIZE);
+
+            //snake
+            for(int i = 0; i < s.getBodyParts(); i++) {
+                if(i==0) {
+                    g.setColor(Color.orange);
+                    g.fillRect(s.getX()[i], s.getY()[i], GameConstant.UNIT_SIZE, GameConstant.UNIT_SIZE);
+                } else {
+                    g.setColor(Color.YELLOW);
+                    g.fillRect(s.getX()[i], s.getY()[i], GameConstant.UNIT_SIZE, GameConstant.UNIT_SIZE);
+                }
+            }
+        }
+    }
+
 }
