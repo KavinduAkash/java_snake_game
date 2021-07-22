ef10630bd69b58c77148a86acddb15c2f93d9b0d
KavinduAkash <bkkasamarasingha1220@gmail.com>
2021-07-22 06:50:57 +0530

    create snake modal class
    snake modal handle by this class. it has special methods to make manage snake moving and reset snake values for new game

---
+++ src/com/swlc/javasnakegame/modal/Snake.java
@@ -0,0 +1,84 @@
+package com.swlc.javasnakegame.modal;
+
+import com.swlc.javasnakegame.constant.GameConstant;
+
+/**
+ * @author hp
+ */
+
+/**
+ * This class responsible for handle snake
+ */
+public class Snake {
+    int x[] = new int[GameConstant.UNIT_SIZE];
+    int y[] = new int[GameConstant.UNIT_SIZE];
+
+    int bodyParts = 1;
+    char direction = 'R';
+
+    /**
+     * This method use to handle snake direction and moving
+     *
+     * Direction keys
+     * U = up
+     * D = down
+     * L = left
+     * R = right
+     */
+    public void move() {
+        for (int i = bodyParts; i > 0; i--) {
+            x[i] = x[i - 1];
+            y[i] = y[i - 1];
+
+            switch (direction) {
+                case 'U':
+                    y[0] = y[0] - GameConstant.UNIT_SIZE;
+                    break;
+                case 'D':
+                    y[0] = y[0] + GameConstant.UNIT_SIZE;
+                    break;
+                case 'L':
+                    x[0] = x[0] - GameConstant.UNIT_SIZE;
+                    break;
+                case 'R':
+                    x[0] = x[0] + GameConstant.UNIT_SIZE;
+                    break;
+            }
+        }
+    }
+
+    public int[] getX() {
+        return x;
+    }
+
+    public int[] getY() {
+        return y;
+    }
+
+    public int getBodyParts() {
+        return bodyParts;
+    }
+
+    public void setBodyParts(int bodyParts) {
+        this.bodyParts = bodyParts;
+    }
+
+    public char getDirection() {
+        return direction;
+    }
+
+    public void setDirection(char direction) {
+        this.direction = direction;
+    }
+
+    /**
+     * This method use to reset snake values when play again
+     */
+    public void resetSnake() {
+        x[0] = 0;
+        y[0] = 0;
+        bodyParts = 1;
+        direction = 'R';
+    }
+
+}
