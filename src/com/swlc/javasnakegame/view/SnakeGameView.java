3cd2354f60b0ff8fbfb9ae1e1d8f6c066c4519a5
KavinduAkash <bkkasamarasingha1220@gmail.com>
2021-07-22 07:33:20 +0530

    set game board and score board to snake game view

--- src/com/swlc/javasnakegame/view/SnakeGameView.java
+++ src/com/swlc/javasnakegame/view/SnakeGameView.java
@@ -1,6 +1,10 @@
 package com.swlc.javasnakegame.view;
 
 import com.swlc.javasnakegame.controller.GameBoard;
+import com.swlc.javasnakegame.controller.ScorePanel;
+import com.swlc.javasnakegame.modal.Food;
+import com.swlc.javasnakegame.modal.Snake;
+import com.swlc.javasnakegame.util.GridBagManager;
 
 import javax.swing.*;
 import java.awt.*;
@@ -9,14 +13,29 @@ import java.awt.*;
  * @author hp
  */
 public class SnakeGameView extends JFrame{
+
+    private Snake snake;
+    private Food food;
+    private GameBoard gameBoard;
+    private ScorePanel scorePanel;
+
     public SnakeGameView() {
         super("Java Snake Game");
         viewBuild();
     }
 
     private void viewBuild() {
-        add(new GameBoard());
+
+        setLayout(new GridBagLayout());
+
+        gameBoard = new GameBoard();
+        scorePanel = new ScorePanel();
+
+        add(gameBoard, new GridBagManager(0, 10, 8, 8));
+        add(scorePanel, new GridBagManager(0, 0, 8, 1));
+
         prepareWindow();
+
     }
 
     private void prepareWindow() {
