395e37c1c94fced2c05053b9048c39e1f26970f4
KavinduAkash <bkkasamarasingha1220@gmail.com>
2021-07-22 11:22:12 +0530

    fix bug at snake

--- src/com/swlc/javasnakegame/modal/Snake.java
+++ src/com/swlc/javasnakegame/modal/Snake.java
@@ -10,8 +10,11 @@ import com.swlc.javasnakegame.constant.GameConstant;
  * This class responsible for handle snake
  */
 public class Snake {
-    int x[] = new int[GameConstant.UNIT_SIZE];
-    int y[] = new int[GameConstant.UNIT_SIZE];
+
+    static final int GAME_UNITS = (GameConstant.SCREEN_HEIGHT * GameConstant.SCREEN_HEIGHT)/GameConstant.UNIT_SIZE;
+
+    int x[] = new int[GAME_UNITS];
+    int y[] = new int[GAME_UNITS];
 
     int bodyParts = 1;
     char direction = 'R';
@@ -29,7 +32,7 @@ public class Snake {
         for (int i = bodyParts; i > 0; i--) {
             x[i] = x[i - 1];
             y[i] = y[i - 1];
-
+        }
             switch (direction) {
                 case 'U':
                     y[0] = y[0] - GameConstant.UNIT_SIZE;
@@ -44,7 +47,6 @@ public class Snake {
                     x[0] = x[0] + GameConstant.UNIT_SIZE;
                     break;
             }
-        }
     }
 
     public int[] getX() {
