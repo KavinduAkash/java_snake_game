a5b0a01b2086a9547d7c927a78143c02bc3869ef
KavinduAkash <bkkasamarasingha1220@gmail.com>
2021-07-22 06:35:31 +0530

    add game constants

---
+++ src/com/swlc/javasnakegame/constant/GameConstant.java
@@ -0,0 +1,12 @@
+package com.swlc.javasnakegame.constant;
+
+/**
+ * @author hp
+ */
+public class GameConstant {
+    public static final int SCREEN_WIDTH = 550;
+    public static final int SCREEN_HEIGHT = 550;
+    public static final int UNIT_SIZE = 25;
+    public static final int DELAY = 200;
+    public static final int SCORE_PANEL_HEIGHT = 60;
+}
