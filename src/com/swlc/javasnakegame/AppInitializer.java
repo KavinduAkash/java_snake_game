922604434357dc070491211c3957e48493ac85f7
KavinduAkash <bkkasamarasingha1220@gmail.com>
2021-07-22 06:32:37 +0530

    create app initializer

---
+++ src/com/swlc/javasnakegame/AppInitializer.java
@@ -0,0 +1,10 @@
+package com.swlc.javasnakegame;
+
+/**
+ * @author hp
+ */
+public class AppInitializer {
+    public static void main(String[] args) {
+        System.out.println("App Initialized!");
+    }
+}
