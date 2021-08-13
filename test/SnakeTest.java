17d79bb1494a2346ef9d34f4c23c74d0ecce1b4c
KavinduAkash <bkkasamarasingha1220@gmail.com>
2021-08-13 06:24:34 +0530

    test snake reset

--- test/SnakeTest.java
+++ test/SnakeTest.java
@@ -18,4 +18,45 @@ public class SnakeTest {
         assertEquals(snakeObj1, snakeObj2);
     }
 
+    /**
+     * When reset snake X[0] should be 0
+     */
+    @Test
+    public void testSnakeResetX() {
+        Snake snakeObj1 = Snake.getInstance();
+        snakeObj1.resetSnake();
+        assertEquals(0, snakeObj1.getX()[0]);
+    }
+
+
+    /**
+     * When reset snake Y[0] should be 0
+     */
+    @Test
+    public void testSnakeResetY() {
+        Snake snakeObj1 = Snake.getInstance();
+        snakeObj1.resetSnake();
+        assertEquals(0, snakeObj1.getY()[0]);
+    }
+
+    /**
+     * When reset snake body parts should be 1
+     */
+    @Test
+    public void testSnakeResetBodyParts() {
+        Snake snakeObj1 = Snake.getInstance();
+        snakeObj1.resetSnake();
+        assertEquals(1, snakeObj1.getBodyParts());
+    }
+
+    /**
+     * When reset snake direction should be 'R'
+     */
+    @Test
+    public void testSnakeResetDirection() {
+        Snake snakeObj1 = Snake.getInstance();
+        snakeObj1.resetSnake();
+        assertEquals('R', snakeObj1.getDirection());
+    }
+
 }
