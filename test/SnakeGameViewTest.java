c93eb07f92207776a4be1d49f7588077e138792c
KavinduAkash <bkkasamarasingha1220@gmail.com>
2021-08-14 07:14:25 +0530

    test snake collision

--- test/SnakeGameViewTest.java
+++ test/SnakeGameViewTest.java
@@ -4,6 +4,7 @@ import com.swlc.javasnakegame.modal.Snake;
 import com.swlc.javasnakegame.view.SnakeGameView;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotSame;
 
 /**
@@ -42,6 +43,50 @@ public class SnakeGameViewTest {
         Food.getInstance().setAppleY(GameConstant.UNIT_SIZE);
         snakeGameView.checkApple();
         assertNotSame(Snake.getInstance().getX()[0], Food.getInstance().getAppleX());
+    }
+
+    /**
+     * test snake body collision
+     */
+    @Test
+    public void testSnakeCollision() {
+        SnakeGameView snakeGameView = new SnakeGameView();
+        Snake instance = Snake.getInstance();
+
+        // body part 1
+        instance.getX()[0] = 200;
+        instance.getY()[0] = 100;
+
+        // body part 2
+        instance.getX()[1] = 225;
+        instance.getY()[1] = 100;
+
+        // body part 3
+        instance.getX()[2] = 250;
+        instance.getY()[2] = 100;
+
+        // body part 4
+        instance.getX()[3] = 250;
+        instance.getY()[3] = 125;
+
+        // body part 5
+        instance.getX()[4] = 225;
+        instance.getY()[4] = 125;
+
+        // body part 6
+        instance.getX()[5] = 200;
+        instance.getY()[5] = 125;
+
+        // body part 7
+        instance.getX()[6] = 200;
+        instance.getY()[6] = 100;
+
+        instance.setBodyParts(7);
+
+        snakeGameView.check();
+
+        assertEquals(false, snakeGameView.getGameBoard().isRunning());
+
     }
 
 }
