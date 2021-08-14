412c35ae501e94afd41067431d83ac7e129d3b21
KavinduAkash <bkkasamarasingha1220@gmail.com>
2021-08-14 06:23:52 +0530

    test game initializing

---
+++ test/SnakeGameViewTest.java
@@ -0,0 +1,18 @@
+import com.swlc.javasnakegame.view.SnakeGameView;
+import org.junit.Test;
+
+/**
+ * @author hp
+ */
+public class SnakeGameViewTest {
+
+    /**
+     * test game initializing
+     */
+    @Test
+    public void testInitSnakeGame() {
+        new SnakeGameView();
+    }
+
+
+}
